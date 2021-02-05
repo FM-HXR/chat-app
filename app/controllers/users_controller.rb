8a4665954fbf9df9743999e017b0e85e653ba29b
FM-HXR <78189593+FM-HXR@users.noreply.github.com>
2021-02-05 11:15:11 +0900

    User info update
    Added update feature to user id info + error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
   def edit
   end
+
+  def update
+    @user.update(params.require(:user).permit(:name, :email))
+    if @user.update(params.require(:user).permit(:name, :email))
+      redirect_to root_path
+    else
+      render edit_user_path
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+  
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
