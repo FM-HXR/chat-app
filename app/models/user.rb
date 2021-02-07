13cc97754050d27b31c460fb5a8eb8e7cf2fd14f
FM-HXR <78189593+FM-HXR@users.noreply.github.com>
2021-02-07 15:05:28 +0900

    Chat room delete + more
    Destroy room along with dependent records
    Changed time display to Tokyo tmz

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 10 }
 
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
   has_many :rooms, through: :room_users
   has_many :messages
 end
